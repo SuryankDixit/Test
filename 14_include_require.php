cdcf513414086f0de11ef20c2c2239c5057b7d95
SuryankDixit <suryankdixit@gmail.com>
2021-07-23 22:19:07 +0530

    include and require

---
+++ 14_include_require.php
@@ -0,0 +1,9 @@
+<?php
+
+// if file does not exist and you include it, warning will come.
+include "_dbconnect.php";
+
+// But if you require it, error will be shown
+require "_dbconnect.php";
+
+?>
