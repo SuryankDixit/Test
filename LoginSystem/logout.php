c67f140d7d218d5de06d5e2dbfe313670efa59ff
SuryankDixit <suryankdixit@gmail.com>
2021-07-25 00:42:32 +0530

    complated logout

--- LoginSystem/logout.php
+++ LoginSystem/logout.php
@@ -1,3 +1,10 @@
+<?php
+    session_start();
+    session_unset();
+    session_destroy();  
+    header("location: login.php");
+    exit;
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +16,6 @@
 </head>
 <body>
     <?php require "partials/nav.php";?>
-
     
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
