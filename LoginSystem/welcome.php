76f696a26181c92d43a19c2fe87c14524fba301a
SuryankDixit <suryankdixit@gmail.com>
2021-07-25 00:43:08 +0530

    Updated

--- LoginSystem/welcome.php
+++ LoginSystem/welcome.php
@@ -3,7 +3,7 @@
     if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin']!=true){
         header("location: login.php");
         exit;
-    }
+    } 
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,7 +16,18 @@
 </head>
 <body>
     <?php require "partials/nav.php";?>
-    <?php echo "Welcome-".$_SESSION['username'] ;?>
+    <!-- <?php echo "Welcome-".$_SESSION['username'] ;?> -->
+
+
+    <div class="container my-4">
+        <div class="alert alert-success" role="alert">
+        <h4 class="alert-heading">Well done!</h4>
+        <p>Aww You are logged in.yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+        <hr>
+        <p class="mb-0">Be sure to logout <a href="/learnphp/LoginSystem/logout.php">using this link</a></p>
+        </div>
+    </div>
+
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
